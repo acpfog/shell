220a273eacff80a472aff04d7d6a14c3f0c0737f
Volodymyr Larkin <acpfog@users.noreply.github.com>
2017-05-04 21:32:32 +0200

    Fix grammar

--- fix_ssh.sh
+++ fix_ssh.sh
@@ -1,4 +1,4 @@
-# ssh-agent stores path to his socket in SSH_AUTH_SOCK variable
+# ssh-agent stores a path to his socket in SSH_AUTH_SOCK variable
 # SSH_AUTH_SOCK in a session in screen should be updated with a new path after each reattachment
 # You can add fixssh() funtion in your .bash_profile and just run fixssh for SSH_AUTH_SOCK update
 #
